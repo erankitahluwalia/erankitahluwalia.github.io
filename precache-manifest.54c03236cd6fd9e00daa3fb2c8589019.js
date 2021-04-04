9d37b6fd963d3a7fde464fe1afdff4b0be7fcf80
Anonymous <go4webcdn@gmail.com>
2021-04-04 22:54:32 +0530

    Initial Commit

---
+++ precache-manifest.54c03236cd6fd9e00daa3fb2c8589019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c4e3da4229bb7acb479e895f19b87c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c84ce4b0c6464dcebe2",
+    "url": "/static/css/main.9010ec04.chunk.css"
+  },
+  {
+    "revision": "486e7775674921c959ad",
+    "url": "/static/js/2.cac0bb94.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.cac0bb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c84ce4b0c6464dcebe2",
+    "url": "/static/js/main.322b1a42.chunk.js"
+  },
+  {
+    "revision": "c78df97410f591e1531c",
+    "url": "/static/js/runtime-main.577318a0.js"
+  }
+]);
